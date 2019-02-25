9ad3df84b83e02061bcc6bdacf2a771049c277b4
Sonja Hohlfeld <sonja.hohlfeld@mayflower.de>
2019-02-25 15:19:54 +0100

    Create new file for todo form

---
+++ client/components/TodoForm.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+function TodoForm({addTodo}) {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (!value) return;
+        addTodo(value)
+        setValue("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                className="input"
+            />
+        </form>
+    )
+}
+
+export default TodoForm;
