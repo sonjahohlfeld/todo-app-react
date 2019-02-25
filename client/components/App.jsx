404e59d2bc32c36c57bcd35befc4722b77d3c771
Sonja Hohlfeld <sonja.hohlfeld@mayflower.de>
2019-02-25 14:35:48 +0100

    Add simple todo list and todo form for adding new todos

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const Todo = ({todo}) => <div className="todo">{todo.text}</div>
+
+function TodoForm({addTodo}) {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (!value) return;
+        addTodo(value)
+        setValue("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                className="input"
+            />
+        </form>
+    )
+}
 
 function App () {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+    const [todos, setTodos] = useState([
+        {text: "Learn React"},
+        {text: "Prepare Lunch"}
+    ])
+
+    const addTodo = text => {
+        const newTodos = [...todos, {text}]
+        setTodos(newTodos)
+    }
+
+    return (
+        <div>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo, index) => (
+                <Todo
+                    key={index}
+                    index={index}
+                    todo = {todo}
+                />
+            ))}       
+        </div>
+    );
 }
 
 export default App;
