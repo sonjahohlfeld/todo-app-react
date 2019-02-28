69d77c349115497302ba72e497df003157e000aa
Sonja Hohlfeld <sonja.hohlfeld@mayflower.de>
2019-02-28 10:01:04 +0100

    Add complete button to todo item

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,21 @@
 import React, {useState} from 'react';
 import TodoForm from './TodoForm.jsx'
 
-const Todo = ({todo, testid}) => <div className="todo" data-testid={testid}>{todo.text}</div>
+const Todo = ({todo, testid}) => <div className="todo" data-testid={testid}>{todo.text} <button data-testid="completeTodo">Complete</button></div>
 
 function App () {
     const [todos, setTodos] = useState([
     ])
 
     const addTodo = text => {
-        const newTodos = [...todos, {text}]
+        const newTodos = [...todos, {text: text, isCompleted: false}]
         setTodos(newTodos)
     }
 
+
     return (
         <div>
-            <h1>My Todo App</h1>
+            <h1 data-testid="appTitle">My Todo App</h1>
             <TodoForm addTodo={addTodo} />
             {todos.map((todo, index) => (
                 <Todo
